be136a9c9ba93df23dbe0de2eee1d7d50ca0c3f0
Thanh Hau Nguyen <thanhhau097@gmail.com>
2020-03-28 19:01:13 +1100

    add template for React web app

---
+++ web/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default App
